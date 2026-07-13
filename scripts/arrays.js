87efe6a228e30547dba56c61557c88bd0c487fd5
Mir Elias Hashimi <elias.h.989@hotmail.com>
2026-07-13 23:08:29 +0200

    changed index.html style.css added arrays.js

---
+++ scripts/arrays.js
@@ -0,0 +1,50 @@
+const photoArray = [
+    {
+        name: "See und Berge mit Eis und Schnee ",
+        link: "./assets/images/1_Pic.jpg"
+    },
+    {
+        name: "Regnerische Stadt bei Nacht",
+        link: "./assets/images/2_Pic.jpg"
+    },
+    {
+        name: "Helle und Dunkle Wolken am Himmel",
+        link: "./assets/images/3_Pic.jpg"
+    },
+    {
+        name: "Grün gelber Vogel auf einem Ast",
+        link: "./assets/images/4_Pic.jpg"
+    },
+    {
+        name: "Wolken Wirbel aus der All ansicht",
+        link: "./assets/images/5_Pic.jpg"
+    },
+    {
+        name: "Fluss am Berg mit trübem Wetter",
+        link: "./assets/images/6_Pic.jpg"
+    },
+    {
+        name: "Ente flattert im Wasser",
+        link: "./assets/images/7_Pic.jpg"
+    },
+    {
+        name: "Ein Mann auf einem Fels",
+        link: "./assets/images/8_Pic.jpg"
+    },
+    {
+        name: "Kleiner weiß brauner Vogel",
+        link: "./assets/images/9_Pic.jpg"
+    },
+    {
+        name: "2 kleine Leopaden auf einem Stein",
+        link: "./assets/images/10_Pic.jpg"
+    },
+    {
+        name: "Mit Schnee bedeckte Gebirge",
+        link: "./assets/images/11_Pic.jpg"
+    },
+    {
+        name: "Ein Baum im Schnee",
+        link: "./assets/images/12_Pic.jpg"
+    }
+];
